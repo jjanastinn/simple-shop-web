1f29fd16476583f5b49de83c4eedd5aca69c0c4a
Jana Stinn <jana-stinn@gmx.de>
2018-08-16 16:02:27 +0200

    initialized react app and fetchdata from api

---
+++ src/services/http-service.js
@@ -0,0 +1,13 @@
+class HttpService {
+  getProducts = () => {
+    var promise = new Promise((resolve, reject) => {
+      fetch('http://localhost:3004/product')
+      .then(res => {
+        resolve(res.json());
+      })
+    });
+    return promise;
+  }
+}
+
+export default HttpService;
